cec743bf6bc218f778a0e4e0bcc2485bf2941a74
Rooby Dartiny <rdartiny19@gmail.com>
2026-05-16 23:46:47 -0400

    website under  maintenance 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import Layout from './components/Layout.jsx';
 import Home from './components/Home.jsx';
-import About from './components/About.jsx';
-import Projects from './components/Projects.jsx';
-import Contact from './components/Contact.jsx';
 
 function App() {
   return (
     <Layout>
       <Home />
-      <About />
-      <Projects />
-      <Contact />
     </Layout>
   );
 }
